509bc9af9201bca1667a3758626ef43bc9f522ec
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-05-12 19:29:14 -0400

    hunter patch update

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,14 +1,14 @@
 hunter_config(SPIRV-Headers
-    VERSION v1.4.1-p0
-    URL https://github.com/mchiasson/SPIRV-Headers/archive/v1.4.1-p0.tar.gz
-    SHA1 942ba385d1f71da73dab5f5467b53b770f954ad5
+    VERSION v1.4.1-p1
+    URL https://github.com/mchiasson/SPIRV-Headers/archive/v1.4.1-p1.tar.gz
+    SHA1 6641b1ff4050c3a7c47f0147bd6db6963848fdc6
     CMAKE_ARGS SPIRV_HEADERS_ENABLE_EXAMPLES=OFF
 )
 
 hunter_config(SPIRV-Tools
-    VERSION v2019.2-p1
-    URL https://github.com/mchiasson/SPIRV-Tools/archive/v2019.2-p1.tar.gz
-    SHA1 e7f2f93a95618d094c653aa11f317b533ac66e6b
+    VERSION v2019.2-p3
+    URL https://github.com/mchiasson/SPIRV-Tools/archive/v2019.2-p3.tar.gz
+    SHA1 ee81baedf0d680d7f78b4b29a40be7e82461b599
     KEEP_PACKAGE_SOURCES
     CMAKE_ARGS SPIRV_SKIP_TESTS=ON SPIRV_WERROR=OFF
 )
@@ -18,5 +18,5 @@ hunter_config(glslang
     URL https://github.com/mchiasson/glslang/archive/v7.11.3188-p3.tar.gz
     SHA1 d515bf21e7a9207e21549314fbcb27c712ee8058
     KEEP_PACKAGE_SOURCES
-    CMAKE_ARGS BUILD_TESTING=OFF
+    CMAKE_ARGS ENABLE_HLSL=ON BUILD_TESTING=OFF
 )
