77b15570776c5154a7ed35cf916eafdd6d54e547
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-10-14 17:28:39 -0400

    Hunterized

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,18 +1,12 @@
 # utility functions
 
 function (shaderc_use_gmock TARGET)
-  target_include_directories(${TARGET} PRIVATE
-    ${gmock_SOURCE_DIR}/include
-    ${gtest_SOURCE_DIR}/include)
-  target_link_libraries(${TARGET} PRIVATE gmock gtest_main)
+  target_link_libraries(${TARGET} PRIVATE GMock::main)
 endfunction(shaderc_use_gmock)
 
 function(shaderc_default_c_compile_options TARGET)
   if (NOT "${MSVC}")
     target_compile_options(${TARGET} PRIVATE -Wall -Werror -fvisibility=hidden)
-    if (NOT "${MINGW}")
-      target_compile_options(${TARGET} PRIVATE -fPIC)
-    endif()
     if (ENABLE_CODE_COVERAGE)
       # The --coverage option is a synonym for -fprofile-arcs -ftest-coverage
       # when compiling.
